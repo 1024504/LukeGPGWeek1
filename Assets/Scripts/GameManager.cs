166f476d715af59c8752b3cca86f0629dcf7a68b
Luke Archbold <91643235+1024504@users.noreply.github.com>
2022-02-15 14:11:40 +1100

    added player class

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,191 +1,122 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-	public int indexNumber;
+	public int playerNumber;
 	public int[] diceRolls;
-	private int dieLocked;
-	//public Vector3 firstDieLocation;
+	public bool isDiceLocked = true;
+	public int noOfDiceLock;
 }
 
 public class GameManager : MonoBehaviour
 {
 	// Even numbers are player 1's turn.
 	public int playerIndex;
+	public int gameTurn = 1;
+	public int totalTurns = 2;
 	public int totalPlayers = 1;
 	public int sidesOfDie = 1;
 	public int totalDice = 1;
 	public Player[] player;
-	
+
 	int Roll()
 	{
-		int roll = Random.Range(1,sidesOfDie+1);
-		
+		int roll = Random.Range(1, sidesOfDie + 1);
+
 		//Reason this is a function is to add in extra effects like spawning/destroying cubes.
-		
+
 		return roll;
 	}
 
 	// Start is called before the first frame update
-    void Start()
-    {
-	    player = new Player[totalPlayers];
-	    for (int i=0; i<totalPlayers;i++)
-	    {
-		    player[i].indexNumber = i;
-		    player[i].diceRolls = new int[totalDice];
-	    }
-	    
-	    Debug.Log("Player 1 goes first.\nPress 'Space' to roll.");
-    }
+	void Start()
+	{
+		player = new Player[totalPlayers];
+		for (int i = 0; i < totalPlayers; i++)
+		{
+			player[i].playerNumber = i+1;
+			player[i].diceRolls = new int[totalDice];
+		}
 
-    // Update is called once per frame
-    void Update()
-    {
-	    if (Input.GetKeyDown(KeyCode.Space))
-	    {
-		    for (int i = 0; i < totalDice; i++)
-		    {
-			    player[playerIndex].diceRolls[i] = Roll();
-			    
-		    }
+		Debug.Log("Player 1 goes first.\nPress 'Space' to roll.");
+	}
 
-		    if (playerIndex == 0)
-		    {
-			    p1Score[0] = Roll(p1A1);
-			    p1Score[1] = Roll(p1A2);
-			    Debug.Log("Player 1 rolled "+p1Score[0]+" and "+p1Score[1]+".\nPlayer 1's score is " + (p1Score[0] + p1Score[1]));
-			    playerIndex += 1;
-			    Debug.Log("Player 2's turn.\nPress 'Enter' to roll.");
-		    }
-		    else if (playerIndex == 2)
-		    {
-			    if (p1Lock == 2)
-			    {
-				    Debug.Log("Player 1's score is " + (p1Score[0] + p1Score[1]));
-				    playerIndex += 1;
-				    Debug.Log("Player 2's turn.\nChoose to lock rolls: neither, first, or both.");
-				    Debug.Log("Press 0, 1, or 2 to lock rolls.\nCurrently "+p1Lock+" rolls are locked.");
-			    }
-			    else if (p1Lock == 1)
-			    {
-				    p1Score[1] = Roll(p1A2);
-				    Debug.Log("Player 1 rolled "+p1Score[1]+".\nPlayer 1's score is " + (p1Score[0] + p1Score[1]));
-				    playerIndex += 1;
-				    Debug.Log("Player 2's turn.\nChoose to lock rolls: neither, first, or both.");
-				    Debug.Log("Press 0, 1, or 2 to lock rolls.\nCurrently "+p1Lock+" rolls are locked.");
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			if (gameTurn <= totalTurns)
+			{
+				if (player[playerIndex].isDiceLocked)
+				{
+					if (player[playerIndex].noOfDiceLock > 0)
+					{
+						// rolls dice for that player
+						for (int j = 0; j < player[playerIndex].noOfDiceLock; j++)
+						{
+							player[playerIndex].diceRolls[j] = Roll();
+						}
 
-			    }
-			    else
-			    {
-				    p1Score[0] = Roll(p1A1);
-				    p1Score[1] = Roll(p1A2);
-				    Debug.Log("Player 1 rolled "+p1Score[0]+" and "+p1Score[1]+".\nPlayer 1's score is " + (p1Score[0] + p1Score[1]));
-				    playerIndex += 1;
-				    Debug.Log("Player 2's turn.\nChoose to lock rolls: neither, first, or both.");
-				    Debug.Log("Press 0, 1, or 2 to lock rolls.\nCurrently "+p1Lock+" rolls are locked.");
-			    }
-		    }
-		    else if (playerIndex == 5)
-		    {
-			    p1Lock = 0;
-			    p2Lock = 0;
-			    playerIndex = 0;
-			    Debug.Log("Player 1 goes first.\nPress 'Space' to roll.");
-		    }
-	    }
+						Debug.Log("Player " + player[playerIndex].playerNumber + " rolled a total of " +
+						          player[playerIndex].diceRolls.Sum());
+					}
+					
+					player[playerIndex].isDiceLocked = false;
+					playerIndex++;
+					if (playerIndex == totalPlayers)
+					{
+						playerIndex = 0;
+						gameTurn++;
+						if (gameTurn > totalTurns)
+						{
+							// Game Over
+							player[0].isDiceLocked = true;
+							
+							//display score and offer to start again.
+						}
+					}
+				}
+				else
+				{
+					player[playerIndex].isDiceLocked = true;
+				}
+			}
+			else
+			{
+				for (int i = 0; i < totalPlayers; i++)
+				{
+					player[i].diceRolls = new int[totalDice];
+				}
 
-	    if (Input.GetKeyDown(KeyCode.KeypadEnter)||Input.GetKeyDown(KeyCode.Return))
-	    {
-		    if (playerIndex == 1)
-		    {
-			    p2Score[0] = Roll(p1A1);
-			    p2Score[1] = Roll(p1A2);
-			    Debug.Log("Player 2 rolled "+p2Score[0]+" and "+p2Score[1]+".\nPlayer 2's score is " + (p2Score[0] + p2Score[1]));
-			    playerIndex += 1;
-			    Debug.Log("Player 1's turn.\nChoose to lock rolls: neither, first, or both.");
-			    Debug.Log("Press 0, 1, or 2 to lock rolls.\nCurrently "+p1Lock+" rolls are locked.");
-		    }
-		    else if (playerIndex == 3)
-		    {
-			    if (p2Lock == 2)
-			    {
-				    Debug.Log("Player 2's score is " + (p2Score[0] + p2Score[1]));
-				    playerIndex += 1;
-			    }
-			    else if (p2Lock == 1)
-			    {
-				    p2Score[1] = Roll(p1A2);
-				    Debug.Log("Player 2 rolled "+p2Score[1]+".\nPlayer 2's score is " + (p2Score[0] + p2Score[1]));
-				    playerIndex += 1;
-			    }
-			    else
-			    {
-				    p2Score[0] = Roll(p1A1);
-				    p2Score[1] = Roll(p1A2);
-				    Debug.Log("Player 2 rolled "+p2Score[0]+" and "+p2Score[1]+".\nPlayer 2's score is " + (p2Score[0] + p2Score[1]));
-				    playerIndex += 1;
-			    }
-			    if ((p2Score[0] + p2Score[1]) > (p1Score[0] + p1Score[1]))
-			    {
-				    Debug.Log("Game Over. Player 2 wins!\nPress 'Space' to reset game.");
+				gameTurn = 1;
+				player[0].isDiceLocked = false;
+			}
+		}
 
-			    }
-			    else if ((p2Score[0] + p2Score[1]) < (p1Score[0] + p1Score[1]))
-			    {
-				    Debug.Log("Game Over. Player 1 wins!\nPress 'Space' to reset game.");
-
-			    }
-			    else
-			    {
-				    Debug.Log("Game Over. The score was a tie!\nPress 'Space' to reset game.");
-			    }
-		    }
-		    
-	    }
-
-	    if (Input.GetKeyDown(KeyCode.Alpha0) || Input.GetKeyDown(KeyCode.Keypad0))
-	    {
-		    if (playerIndex == 2)
-		    {
-			    p1Lock = 0;
-			    Debug.Log(p1Lock+" rolls are locked.\nPress 'Space' to roll, or choose again.");
-		    }
-		    else if (playerIndex == 3)
-		    {
-			    p2Lock = 0;
-			    Debug.Log(p2Lock+" rolls are locked.\nPress 'Space' to roll, or choose again.");
-		    }
-	    }
-	    
-	    if (Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Keypad1))
-	    {
-		    if (playerIndex == 2)
-		    {
-			    p1Lock = 1;
-			    Debug.Log(p1Lock+" rolls are locked.\nPress 'Space' to roll, or choose again.");
-		    }
-		    else if (playerIndex == 3)
-		    {
-			    p2Lock = 1;
-			    Debug.Log(p2Lock+" rolls are locked.\nPress 'Space' to roll, or choose again.");
-		    }
-	    }
-	    
-	    if (Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Keypad2))
-	    {
-		    if (playerIndex == 2)
-		    {
-			    p1Lock = 2;
-			    Debug.Log(p1Lock+" rolls are locked.\nPress 'Space' to roll, or choose again.");
-		    }
-		    else if (playerIndex == 3)
-		    {
-			    p2Lock = 2;
-			    Debug.Log(p2Lock+" rolls are locked.\nPress 'Space' to roll, or choose again.");
-		    }
-	    }
-    }
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			if (!player[playerIndex].isDiceLocked)
+			{
+				if (player[playerIndex].noOfDiceLock < totalDice)
+				{
+					player[playerIndex].noOfDiceLock++;
+				}
+			}
+		}
+		
+		if (Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			if (!player[playerIndex].isDiceLocked)
+			{
+				if (player[playerIndex].noOfDiceLock < 0)
+				{
+					player[playerIndex].noOfDiceLock--;
+				}
+			}
+		}
+	}
 }
